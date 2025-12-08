166abeaa4d561ba0187dcb43bae8cac416737132
Niel Cansino <nielcansino@gmail.com>
2025-12-08 14:22:23 +0900

    debugging

--- se3cnn/batchnorm.py
+++ se3cnn/batchnorm.py
@@ -183,7 +183,6 @@ class SE3BNConvolution(torch.nn.Module):
         ix = 0
         irm = 0
         irv = 0
-        print(self.Rs)
         for m, d in self.Rs:
             field = input[: ix: ix + m * d]  # [batch, feature * repr, x, y, z]
             ix += m * d
